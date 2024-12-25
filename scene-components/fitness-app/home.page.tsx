8d648729911ad1184d9aa392388f17e9ee42d92d
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-12-25 08:07:05 +0700

    cso

--- scene-components/fitness-app/home.page.tsx
+++ scene-components/fitness-app/home.page.tsx
@@ -3,7 +3,8 @@ import {
     Home, 
     Fence,
     IdCard, 
-    User } from "lucide-react-native";
+    User 
+} from "lucide-react-native";
 import { StatusBar } from "react-native";
 import MobileBottomTabs from "./MobileBottomTabs";
 import React from "react";
@@ -37,7 +38,7 @@ const HomePage = () => {
             <Box className="flex-1">
                 <StatusBar hidden={false} translucent={true} backgroundColor={'transparent'}/>
                 <Box className="flex-1">
-                    <Explorepage setActiveTab={setActiveTab} activeTab={activeTab} />
+                    <Explorepage setActiveTab={setActiveTab} activeTab={activeTab}/>
                 </Box>
                 <Box className="h-[72px] items-center w-full flex md:hidden border-t border-outline-50">
                     <MobileBottomTabs
