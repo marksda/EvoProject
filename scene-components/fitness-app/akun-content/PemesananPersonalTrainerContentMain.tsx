c5d63a2d1401be8d4484d944fb05c54180bbe40f
Syaiful Dwi Anwar <vlack123@gmail.com>
2025-01-05 00:37:03 +0700

    cso

--- scene-components/fitness-app/akun-content/PemesananPersonalTrainerContentMain.tsx
+++ scene-components/fitness-app/akun-content/PemesananPersonalTrainerContentMain.tsx
@@ -3,14 +3,25 @@ import { ScrollView } from "react-native";
 import _ from "lodash"
 import { VStack } from "@/components/ui/vstack";
 import { Pressable } from "@/components/ui/pressable";
-import { Icon } from "@/components/ui/icon";
-import { MapPinIcon } from "lucide-react-native";
+import { AddIcon, Icon } from "@/components/ui/icon";
 import { Text } from "@/components/ui/text";
 import { HStack } from "@/components/ui/hstack";
+import { GlobeIcon, MapPinIcon } from "lucide-react-native";
+import { Portal } from "@/components/ui/portal";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Popover, PopoverBackdrop, PopoverBody, PopoverContent } from "@/components/ui/popover";
+import { Menu, MenuItem, MenuItemLabel } from "@/components/ui/menu";
 
 
 const PemesananPersonalTrainerContentMain = () => {
-    
+    const [showMenuPilihKlub, setShowMenuPilihKlub] = React.useState(false);
+
+    const handleOpen = () => {
+        setShowMenuPilihKlub(true)
+      }
+      const handleClose = () => {
+        setShowMenuPilihKlub(false)
+      }
 
     return (
         <>
@@ -18,14 +29,34 @@ const PemesananPersonalTrainerContentMain = () => {
                 horizontal={false}
                 scrollEventThrottle={50}
             >
-                <VStack className="p-1 gap-1">
-                    <Pressable className="bg-gray-300 p-2">
-                        <HStack className="gap-1">
-                            <Icon as={MapPinIcon} />
-                            <Text>- Pilih klub -</Text>
-                        </HStack>
-                    </Pressable>
-                    <Text size="sm">Klub terbilih akan dijadikan sebagai kelas bawaan dan personal trainer</Text>
+                <VStack className="p-2 gap-1">  
+                    <Menu
+                        placement="bottom left"
+                        trigger={(triggerProps) => {
+                            return (
+                                <Pressable 
+                                    {...triggerProps}
+                                    className="bg-gray-300 p-2 shadow-sm"
+                                >
+                                    <HStack className="gap-1">
+                                        <Icon as={MapPinIcon} />
+                                        <Text>- Pilih klub -</Text>
+                                    </HStack>
+                                </Pressable>
+                            )
+                        }}
+                        className="top-[-22px] rounded-none shadow-none bg-slate-50"
+                    >
+                        <MenuItem key="Add account" textValue="Add account">
+                            <Icon as={AddIcon} size="sm" className="mr-2" />
+                            <MenuItemLabel size="sm">Add account</MenuItemLabel>
+                        </MenuItem>
+                        <MenuItem key="Community" textValue="Community">
+                            <Icon as={GlobeIcon} size="sm" className="mr-2" />
+                            <MenuItemLabel size="sm">Community</MenuItemLabel>
+                        </MenuItem>
+                    </Menu>
+                    <Text size="sm">Klub terpilih dijadikan kelas bawaan dan personal trainer</Text>
                 </VStack>
             </ScrollView>
         </>
