f16c6ed988fac53f5f8080cc15f523c4b1023a53
Syaiful Dwi Anwar <vlack123@gmail.com>
2025-01-25 12:24:45 +0700

    cso

--- scene-components/fitness-app/formulir/LoginForm.tsx
+++ scene-components/fitness-app/formulir/LoginForm.tsx
@@ -3,42 +3,45 @@ import { FormControl, FormControlError, FormControlErrorIcon, FormControlErrorTe
 import { AlertCircleIcon } from "@/components/ui/icon";
 import { Input, InputField } from "@/components/ui/input";
 import { VStack } from "@/components/ui/vstack";
+import { ScrollView } from "react-native";
 
 const LoginForm = () => {
   return (    
-    <VStack className="w-[65%] self-center mt-9">      
-        <FormControl
-          isInvalid={false}
-          size="md"
-          isDisabled={false}
-          isReadOnly={false}
-          isRequired={false}          
-        >
-          <FormControlLabel>
-            <FormControlLabelText>Password</FormControlLabelText>
-          </FormControlLabel>
-          <Input className="my-1">
-            <InputField
-              type="password"
-              placeholder="password"
-            />
-          </Input>
-          <FormControlHelper>
-            <FormControlHelperText>
-              Minimal 3 karakter.
-            </FormControlHelperText>
-          </FormControlHelper>
-          <FormControlError>
-            <FormControlErrorIcon as={AlertCircleIcon} />
-            <FormControlErrorText>
-              Atleast 6 characters are required.
-            </FormControlErrorText>
-          </FormControlError>
-        </FormControl>
-        <Button className="w-fit self-end mt-4" size="sm">
-          <ButtonText>Submit</ButtonText>
-        </Button>
-    </VStack>
+    <ScrollView>
+      <VStack className="w-[65%] self-center mt-9">      
+          <FormControl
+            isInvalid={false}
+            size="md"
+            isDisabled={false}
+            isReadOnly={false}
+            isRequired={false}          
+          >
+            <FormControlLabel>
+              <FormControlLabelText>Password</FormControlLabelText>
+            </FormControlLabel>
+            <Input className="my-1">
+              <InputField
+                type="password"
+                placeholder="password"
+              />
+            </Input>
+            <FormControlHelper>
+              <FormControlHelperText>
+                Minimal 3 karakter.
+              </FormControlHelperText>
+            </FormControlHelper>
+            <FormControlError>
+              <FormControlErrorIcon as={AlertCircleIcon} />
+              <FormControlErrorText>
+                Atleast 6 characters are required.
+              </FormControlErrorText>
+            </FormControlError>
+          </FormControl>
+          <Button className="w-fit self-end mt-4" size="sm">
+            <ButtonText>Submit</ButtonText>
+          </Button>
+      </VStack>
+    </ScrollView>
   );
 }
 
