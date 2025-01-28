b047f9ce923473da014a63ae411b6173651a39c5
Syaiful Dwi Anwar <vlack123@gmail.com>
2025-01-28 10:25:58 +0700

    cso

--- scene-components/fitness-app/akun-content/AkunContentMain.tsx
+++ scene-components/fitness-app/akun-content/AkunContentMain.tsx
@@ -14,14 +14,16 @@ import { BackHandler } from "react-native";
 import { AlertDialog, AlertDialogBackdrop, AlertDialogBody, AlertDialogContent } from "@/components/ui/alert-dialog";
 import { Icon } from "@/components/ui/icon";
 import { TriangleAlertIcon } from "lucide-react-native";
-import { useAppSelector } from "@/features/ssot/hook";
+import { useAppDispatch } from "@/features/ssot/hook";
+import { resetProfile } from "@/services/profile-slice";
+import { resetToken } from "@/services/token-slice";
 
 const exitApp = () => {
-    BackHandler.exitApp();
+  BackHandler.exitApp();
 }
 
 const AkunContentMain = () => {
-  const token = useAppSelector(state => state.persisted.token);
+  const dispatch = useAppDispatch();
   const navigation = useNavigation();
   const [tmblPaket, setTmblPaket] = React.useState('membership');
   const [showDetailPaket, setShowDetailPaket] = React.useState(false);
@@ -33,6 +35,8 @@ const AkunContentMain = () => {
         setShowAlertDialog(true);
         break;    
       case "Keluar":
+        dispatch(resetProfile(null));
+        dispatch(resetToken(null));
         exitApp();
         break;               
       default:
@@ -47,133 +51,133 @@ const AkunContentMain = () => {
   return (
     <>
       <Box className="m-1 p-1">
-          <Box className="rounded-md shadow-sm border-l-[4px] border-l-biru p-2 bg-slate-100">
-              <Text size="xs">Rekomendasi teman anda dan dapatkan FW coin</Text>
-              <Text size="sm" bold={true} className="text-biru antialiased">Perbarui profil anda untuk mendapatkan kode referal</Text>
-          </Box>
-          <Divider className="my-2"/>
-          <VStack space="md">
-              <HStack className="justify-between">
-                  <Text size="sm" bold={true} className="text-biru antialiased">Paket</Text>
-                  <Pressable
-                      onPress={() => {
-                        setShowDetailPaket((prev) => !prev);
-                      }}
-                  >
-                      <Text size="sm" bold={true} className="text-biru antialiased">Lihat Paket</Text>
-                  </Pressable>
-              </HStack>
-              <HStack space="xs">
-                  <Pressable 
-                      className={
-                          `border ${tmblPaket == "membership" ? "bg-biru" : "bg-white border-biru"} py-1 px-5 rounded-3xl`
-                      }
-                      onPress={() => {
-                          setTmblPaket('membership');
-                      }}
-                  >
-                      <Text 
-                          size="sm" 
-                          className={
-                              `${tmblPaket == "membership" ? "text-white": "text-biru"} subpixel-antialiased`
-                          }
-                      >
-                          Membership
-                      </Text>
-                  </Pressable>
-                  <Pressable 
-                      className={
-                          `border ${tmblPaket == 'personaltraining' ? "bg-biru" : "bg-white border border-biru"} py-1 px-5 rounded-3xl`
-                      }
-                      onPress={() => {
-                          setTmblPaket('personaltraining');
-                      }}
-                  >
-                      <Text 
-                          size="sm" 
-                          className={
-                              `${tmblPaket == 'personaltraining' ? 'text-white': 'text-biru'} subpixel-antialiased`
-                          }
-                      >
-                          Personal Training
-                      </Text>
-                  </Pressable>
-              </HStack>
-              {
-                tmblPaket == "membership" ? <MembershipCard /> : <PersonalTraining />
-              }
-              <Divider className="my-2"/>
-              <Text size="sm" bold={true} className="antialiased text-biru">Aktifitas</Text>
-              <AccordionBox 
-                  title="Pemesanan Kelas" 
-                  variant="Kuning"
-                  setActionPress={handleAcordionBoxPress}
-              />
-              <AccordionBox 
-                  title="Pemesanan Personal Trainer" 
-                  variant="Kuning"
-                  setActionPress={handleAcordionBoxPress}
-              />
-              <AccordionBox 
-                  title="Tugas Harian" 
-                  variant="Kuning"
-                  setActionPress={handleAcordionBoxPress}
-              />
-              <Divider className="my-2"/>
-              <Text size="sm" bold={true} className="antialiased text-biru">Riwayat</Text>
-              <AccordionBox 
-                  title="Riwayat" 
-                  variant="Biru"
-                  setActionPress={handleAcordionBoxPress}
-              />
-              <AccordionBox 
-                  title="Tagihan" 
-                  variant="Biru"
-                  setActionPress={handleAcordionBoxPress}
-              />
-              <AccordionBox 
-                  title="Bonus" 
-                  variant="Biru"
-                  setActionPress={handleAcordionBoxPress}
-              />
-              <Divider className="my-2"/>
-              <AccordionBox 
-                  title={token.token != null ? 'Pengaturan' : 'Buat Akun'}
-                  variant="Merah"
-                  setActionPress={handleAcordionBoxPress}
-              />
-              <AccordionBox 
-                  title="Keluar" 
-                  variant="Merah"
-                  setActionPress={handleAcordionBoxPress}
-              />
-          </VStack>
+        <Box className="rounded-md shadow-sm border-l-[4px] border-l-biru p-2 bg-slate-100">
+          <Text size="xs">Rekomendasi teman anda dan dapatkan FW coin</Text>
+          <Text size="sm" bold={true} className="text-biru antialiased">Perbarui profil anda untuk mendapatkan kode referal</Text>
+        </Box>
+        <Divider className="my-2"/>
+        <VStack space="md">
+            <HStack className="justify-between">
+                <Text size="sm" bold={true} className="text-biru antialiased">Paket</Text>
+                <Pressable
+                    onPress={() => {
+                      setShowDetailPaket((prev) => !prev);
+                    }}
+                >
+                    <Text size="sm" bold={true} className="text-biru antialiased">Lihat Paket</Text>
+                </Pressable>
+            </HStack>
+            <HStack space="xs">
+                <Pressable 
+                    className={
+                        `border ${tmblPaket == "membership" ? "bg-biru" : "bg-white border-biru"} py-1 px-5 rounded-3xl`
+                    }
+                    onPress={() => {
+                        setTmblPaket('membership');
+                    }}
+                >
+                    <Text 
+                        size="sm" 
+                        className={
+                            `${tmblPaket == "membership" ? "text-white": "text-biru"} subpixel-antialiased`
+                        }
+                    >
+                        Membership
+                    </Text>
+                </Pressable>
+                <Pressable 
+                    className={
+                        `border ${tmblPaket == 'personaltraining' ? "bg-biru" : "bg-white border border-biru"} py-1 px-5 rounded-3xl`
+                    }
+                    onPress={() => {
+                        setTmblPaket('personaltraining');
+                    }}
+                >
+                    <Text 
+                        size="sm" 
+                        className={
+                            `${tmblPaket == 'personaltraining' ? 'text-white': 'text-biru'} subpixel-antialiased`
+                        }
+                    >
+                        Personal Training
+                    </Text>
+                </Pressable>
+            </HStack>
+            {
+              tmblPaket == "membership" ? <MembershipCard /> : <PersonalTraining />
+            }
+            <Divider className="my-2"/>
+            <Text size="sm" bold={true} className="antialiased text-biru">Aktifitas</Text>
+            <AccordionBox 
+              title="Pemesanan Kelas" 
+              variant="Kuning"
+              setActionPress={handleAcordionBoxPress}
+            />
+            <AccordionBox 
+              title="Pemesanan Personal Trainer" 
+              variant="Kuning"
+              setActionPress={handleAcordionBoxPress}
+            />
+            <AccordionBox 
+              title="Tugas Harian" 
+              variant="Kuning"
+              setActionPress={handleAcordionBoxPress}
+            />
+            <Divider className="my-2"/>
+            <Text size="sm" bold={true} className="antialiased text-biru">Riwayat</Text>
+            <AccordionBox 
+              title="Riwayat" 
+              variant="Biru"
+              setActionPress={handleAcordionBoxPress}
+            />
+            <AccordionBox 
+              title="Tagihan" 
+              variant="Biru"
+              setActionPress={handleAcordionBoxPress}
+            />
+            <AccordionBox 
+              title="Bonus" 
+              variant="Biru"
+              setActionPress={handleAcordionBoxPress}
+            />
+            <Divider className="my-2"/>
+            <AccordionBox 
+              title="Pengaturan"
+              variant="Merah"
+              setActionPress={handleAcordionBoxPress}
+            />
+            <AccordionBox 
+              title="Keluar" 
+              variant="Merah"
+              setActionPress={handleAcordionBoxPress}
+            />
+        </VStack>
       </Box>
       <MobileSidebarActionsheet
         actionsheetVisible={showDetailPaket}
         setActionsheetVisible={setShowDetailPaket}
       />
       <AlertDialog
-          isOpen={showAlertDialog} 
-          onClose={handleCloseAlertDialog} 
-          size="md"
+        isOpen={showAlertDialog} 
+        onClose={handleCloseAlertDialog} 
+        size="md"
       >
-          <AlertDialogBackdrop />
-          <AlertDialogContent>
-              <AlertDialogBody className="mt-3 mb-4">
-                  <VStack space="md">
-                      <Icon 
-                          as={TriangleAlertIcon} 
-                          // @ts-ignore
-                          size={48} 
-                          className="color-yellow-400 self-center"
-                      />
-                      <Text size="sm" bold={true} className="text-center">
-                          Fitur belum tersedia, tunggu kehadirannya sebentar lagi.
-                      </Text>
-                  </VStack>
-              </AlertDialogBody>
-          </AlertDialogContent>
+        <AlertDialogBackdrop />
+        <AlertDialogContent>
+          <AlertDialogBody className="mt-3 mb-4">
+            <VStack space="md">
+              <Icon 
+                as={TriangleAlertIcon} 
+                // @ts-ignore
+                size={48} 
+                className="color-yellow-400 self-center"
+              />
+              <Text size="sm" bold={true} className="text-center">
+                Fitur belum tersedia, tunggu kehadirannya sebentar lagi.
+              </Text>
+            </VStack>
+          </AlertDialogBody>
+        </AlertDialogContent>
       </AlertDialog>
     </>
   );
