d9f1e53058a233b1756d4551d17986e90a45f24f
Syaiful Dwi Anwar <vlack123@gmail.com>
2025-02-12 13:31:32 +0700

    cso

--- scene-components/fitness-app/beranda-content/BerandaContentHeader.tsx
+++ scene-components/fitness-app/beranda-content/BerandaContentHeader.tsx
@@ -11,7 +11,6 @@ import { ChevronDown } from "lucide-react-native";
 
 const BerandaContentHeader = ({ setActionsheetVisible }: any) => {
   const handleOpenActionSheet = () => {
-    console.log('open');
     setActionsheetVisible(true);
   };
 
