4d81735fe8ccd89deca65ca9032ca9c8a5961120
marksda <vlack123@gmail.com>
2025-03-13 11:19:51 +0700

    cso

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset', "nativewind/babel"],
 
-  plugins: [["module-resolver", {
-    root: ["./"],
-    extensions: [".js", ".ts", ".tsx", ".jsx"],
-
-    alias: {
-      "@": "./",
-      "tailwind.config": "./tailwind.config.js"
-    }
-  }]]
+  plugins: [
+    [
+      "module-resolver", 
+      {
+        root: ["./"],
+        extensions: [".js", ".ts", ".tsx", ".jsx"],
+        alias: {
+          "@": "./",
+          "tailwind.config": "./tailwind.config.js"
+        }
+      }
+    ],
+    'react-native-reanimated/plugin' 
+  ]
 };
